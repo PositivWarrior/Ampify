868bac6a420c460ea80bfb94ad74231fc2dfe037
PositivWarrior <kacpermargol@gmail.com>
2024-12-09 12:32:16 +0100

    Header/Log In/ Sign Up add

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,5 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div className="text-green-500">Main Contenet</div>
+    <div className="bg-neutral-900 rounded-lg w-full h-full overflow-hidden overflow-y-auto">
+      <Header />
+    </div>
   )
 }
